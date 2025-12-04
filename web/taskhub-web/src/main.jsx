b7ade6340ccb084b2235861369e89c46d16d0070
Tfangz <tfangz888@gmail.com>
2025-12-04 17:24:31 +0800

    Update main.jsx

--- web/taskhub-web/src/main.jsx
+++ web/taskhub-web/src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ConfigProvider, App as AntdApp, theme as antdTheme } from 'antd'
+import zhCN from 'antd/locale/zh_CN'
+import 'antd/dist/reset.css'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ConfigProvider
+      locale={zhCN}
+      theme={{
+        algorithm: antdTheme.defaultAlgorithm, // 可切换为 antdTheme.darkAlgorithm
+        token: {
+          colorPrimary: '#1677ff',
+          borderRadius: 6,
+          fontSize: 14
+        }
+      }}
+    >
+      <AntdApp>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AntdApp>
+    </ConfigProvider>
+  </React.StrictMode>
 )
